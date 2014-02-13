a49e4b37fd83d5f3c1290980d13b90f6ffcb69e1
karaschee <baishihaojc@gmail.com>
2014-02-13 18:28:53 +0800

    update

---
+++ dist/tinytip-debug.js
@@ -0,0 +1,38 @@
+define("karaschee/tinytip/1.0.0/tinytip-debug", [ "$-debug", "./tinytip-debug.css" ], function(require, exports, module) {
+    var $ = require("$-debug");
+    require("./tinytip-debug.css");
+    var defaults = {
+        txt: "操作进行中，请稍候..."
+    };
+    var Tinytip = function(options) {
+        this.options = $.extend({}, defaults, options);
+        // Initialize
+        var wrapper = $('<div class="tinytip_wrapper"></div>');
+        wrapper.html(options.txt);
+        this.wrapper = wrapper;
+        Tinytip.tips = Tinytip.tips || [];
+        Tinytip.tips.push(this);
+    };
+    Tinytip.hide = function() {
+        $.each(Tinytip.tips, function(i, v) {
+            $(v).hide();
+        });
+    };
+    $.extend(Tinytip.prototype, {
+        show: function(txt) {
+            this.wrapper.html(txt);
+            this.wrapper.show();
+        },
+        hide: function() {
+            this.wrapper.hide();
+        },
+        reset: function() {
+            this.wrapper.html(this.options.txt);
+        }
+    });
+    module.exports = Tinytip;
+});
+
+define("karaschee/tinytip/1.0.0/tinytip-debug.css", [], function() {
+    seajs.importStyle(".tinytip_wrapper{position:fixed;top:0;left:50%;margin-left:-75px;width:154px;padding:10px 10px 10px 30px;font-weight:700;font-size:12px;color:#666;background:url(/images/spinners/octocat-spinner-16px.gif?c9b3efa2) 10px 50% no-repeat #eee;border:1px solid #ddd;border-top:1px solid #fff;border-radius:5px;border-top-left-radius:0;border-top-right-radius:0;z-index:20}");
+});
--- src/tinytip.js
+++ src/tinytip.js
@@ -1,7 +1,44 @@
 define(function(require, exports, module) {
 
-  var tinytip;
+  var $ = require("$");
+  require("./tinytip.css");
 
-  module.exports = tinytip;
+  var defaults = {
+    txt: '操作进行中，请稍候'
+  };
 
+  var Tinytip = function(options){
+    this.options = $.extend({}, defaults, options);
+
+    // Initialize
+    var wrapper = $('<div style="display:none;" class="tinytip_wrapper"></div>');
+    wrapper.html(this.options.txt);
+    $('body').append(wrapper);
+
+    this.wrapper = wrapper;
+
+    Tinytip.tips = Tinytip.tips || [];
+    Tinytip.tips.push(this);
+  };
+
+  Tinytip.hide = function(){
+    $.each(Tinytip.tips, function(i, v){
+      $(v).hide();
+    });
+  }
+
+  $.extend(Tinytip.prototype, {
+    show: function(txt){
+      this.wrapper.html(txt);
+      this.wrapper.show();
+    },
+    hide: function(){
+      this.wrapper.hide();
+    },
+    reset: function(){
+      this.wrapper.html(this.options.txt);
+    }
+  });
+
+  module.exports = Tinytip;
 });
